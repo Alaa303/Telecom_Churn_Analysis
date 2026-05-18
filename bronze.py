7da9275e88f49134045fa8c1f62c038272b77c42
Alaa Mahdy <aloaa9865@gmail.com>
2026-05-19 00:12:21 +0300

    Initial commit: Setup Medallion Architecture and Power BI Dashboard

---
+++ bronze.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def bronze():
+    df = pd.read_csv("telecom_customer_churn.csv")
+    df.columns = df.columns.str.replace(" ", "_").str.lower()
+    df.to_csv("bronze.csv", index=False)
+    return df
+
+
+if __name__ == "__main__":
+    bronze()
